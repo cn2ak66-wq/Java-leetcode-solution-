c8b94c1b2afff6c3de5d5a02b69d270ad57584b9
Anup Kumar Gupta <cn2ak66@gmail.com>
2026-09-24 15:58:06 +0530

    Time: 19 ms (93.09%), Space: 57.1 MB (74.17%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -2,18 +2,14 @@ class RecentCounter {
     private int[] queue = new int[10001];
     private int front = 0;
     private int rear = 0;
-
     public RecentCounter() {
     }
-
     public int ping(int t) {
         queue[rear] = t;
         rear++;
-
         while (queue[front] < t - 3000) {
             front++;
         }
-
         return rear - front;
     }
 }
